906b1e0c198c7f988fba277592ef71a742c84694
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-17 13:15:06 -0700

    updated api delete route, removes category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,6 +55,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
