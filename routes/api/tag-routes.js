04778177fafebc2c19895f696bd978ae861aaf17
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-17 20:41:49 -0700

    updated tag api route to update name based on id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,6 +43,16 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await EventTarget.find(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
