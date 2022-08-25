2ad8bcc1612c78247f6ccb608c583003f57f56eb
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-25 00:23:48 -0700

    final push, bugs fixed

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mysql = require("mysql2");
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
+
+// Open the connection to MySQL server
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysql",
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
